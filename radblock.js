86ee40349c9778431cc9f37089914b81e7b0997b
Andrew Monks <a@monks.co>
2016-04-28 14:01:35 -0500

    added image replacer

--- radblock.js
+++ radblock.js
@@ -1,1 +1,5 @@
-console.log('hello!')
+var array_of_images = Array.prototype.slice.call(document.getElementsByTagName('img'))
+array_of_images.map(function (img) {
+  img.setAttribute('src', chrome.extension.getURL('./icons/icon-48.png'))
+})
+
